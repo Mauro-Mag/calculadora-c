cc33c8e929c0b5cbe1fc70cade80cb63b11bf85e
Mauro-Mag <tuemail@example.com>
2026-04-25 17:06:46 -0300

    correccion printfs sin \n

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main()
     printf("Calculadora en C\n");
     printf("%.2f elevado a %.2f = %.2f\n", a, b, potencia(a, b));
     printf("raiz %.2f de %.2f = %.2f\n", a, b, raiz(a, b));
-    printf("%.2f * %.2f = %.2f", a, b, multiplicar(a, b));
-    printf("%.2f / %.2f = %.2f", a, b, dividir(a, b));
+    printf("%.2f * %.2f = %.2f\n", a, b, multiplicar(a, b));
+    printf("%.2f / %.2f = %.2f\n", a, b, dividir(a, b));
     printf("%.2f + %.2f = %.2f\n", a, b, suma(a, b));
     printf("%.2f - %.2f = %.2f\n", a, b, resta(a, b));
     return 0;
