90d52e23682f9ebae67d84fbf8aadd3b4d6a8ddd
Mauro-Mag <tuemail@example.com>
2026-04-25 17:05:12 -0300

    recorreccion de funciones en headers y viceversa

--- funciones.c
+++ funciones.c
@@ -1,9 +1,32 @@
 #include "funciones.h"
 
-float potencia(float base, float exponente);
-float raiz(float numero, float indice);
-float multiplicar (float a, float b);
-float dividir (float a, float b);
-float suma(float a, float b);
-float resta(float a, float b);
 
+float potencia(float base, float exponente)
+{
+    return pow(base, exponente);
+}
+
+float raiz(float numero, float indice)
+{
+    return pow(numero, 1.0 / indice);
+}
+
+float multiplicar(float a, float b)
+{
+    return a*b;
+}
+
+float dividir(float a, float b)
+{
+    return a/b;
+}
+
+float suma(float a, float b)
+{
+    return a+b;
+}
+
+float resta(float a, float b)
+{
+    return a-b;
+}
--- funciones.h
+++ funciones.h
@@ -2,34 +2,13 @@
 #define FUNCIONES_H_INCLUDED
 #include <math.h>
 
-float potencia(float base, float exponente)
-{
-    return pow(base, exponente);
-}
+float potencia(float base, float exponente);
+float raiz(float numero, float indice);
+float multiplicar (float a, float b);
+float dividir (float a, float b);
+float suma(float a, float b);
+float resta(float a, float b);
 
-float raiz(float numero, float indice)
-{
-    return pow(numero, 1.0, indice);
-}
 
-float multiplicar(float a, float b)
-{
-    return a*b;
-}
-
-float dividir(float a, float b)
-{
-    return a/b;
-}
-
-float suma(float a, float b)
-{
-    return a+b;
-}
-
-float resta(float a, float b)
-{
-    return a-b;
-}
 
 #endif // FUNCIONES_H_INCLUDED
